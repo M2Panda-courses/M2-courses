8ceb3ab0164df1321dbed0bc4c7c4b4c08235eec
M2Panda-courses <panda1445@rambler.ru>
2023-05-03 20:48:29 +0300

    projest

---
+++ src/app/code/Gold/FIsh/registration.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Gold_Fish',
+    __DIR__,
+);
+
