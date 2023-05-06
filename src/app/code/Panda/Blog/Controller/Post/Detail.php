110bd5647a7a81a6facefba42a6f0569ce7557a2
M2Panda-courses <panda1445@rambler.ru>
2023-05-06 20:21:04 +0300

    Magento courses part 1 lessons 5 довив вивід інфи ро отону сесію

--- src/app/code/Panda/Blog/Controller/Post/Detail.php
+++ src/app/code/Panda/Blog/Controller/Post/Detail.php
@@ -2,12 +2,22 @@
 
 namespace Panda\Blog\Controller\Post;
 
+use Magento\Customer\Model\Session;
 use Magento\Framework\App\Action\HttpGetActionInterface;
 
 class Detail implements HttpGetActionInterface
 {
+    private $session;
+    public function __construct(
+        Session $session,
+    ){
+        $this->session = $session;
+    }
+
     public function execute()
     {
-        die ('Blog Post detail');
+        echo '<pre>';
+        var_dump($this->session->getData());
+        die();
     }
 }
---
+++ src/app/code/Panda/Blog/Controller/Post/ListAction.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace Panda\Blog\Controller\Post;
+
+use Magento\Framework\App\Action\HttpGetActionInterface;
+
+class ListAction implements HttpGetActionInterface
+{
+    public function execute()
+    {
+        die ('Blog Post list');
+    }
+}
