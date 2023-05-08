afaf2bea33f0e59ceb5b77547678955fd1e4f858
M2Panda-courses <panda1445@rambler.ru>
2023-05-08 13:11:23 +0300

    Magento courses part 1 lessons 9+10 Created Model, ViewModel, webapi, db data patch, add display data from db on a page

--- src/app/code/Panda/Blog/Model/Post.php
+++ src/app/code/Panda/Blog/Model/Post.php
@@ -3,11 +3,37 @@
 namespace Panda\Blog\Model;
 
 use Magento\Framework\Model\AbstractModel;
+use Panda\Blog\Api\Data\PostInterface;
 
-class Post extends AbstractModel
+class Post extends AbstractModel implements PostInterface
 {
     protected function _construct()
     {
         $this->_init(ResourceModel\Post::class);
+    }
+
+    public function getCreatedAt()
+    {
+        return $this->getData(self::CREATED_AT);
+    }
+
+    public function getTitle()
+    {
+        return $this->getData(self::TITLE);
+    }
+
+    public function setTitle($title)
+    {
+        $this->setData(self::TITLE, $title);
+    }
+
+    public function getContent()
+    {
+        return $this->getData(self::CONTENT);
+    }
+
+    public function setContent($content)
+    {
+        $this->setData(self::CONTENT, $content);
     }
 }
