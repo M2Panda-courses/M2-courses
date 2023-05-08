3cae134b86976ed8da1b6620fe98b14e9a61ff55
M2Panda-courses <panda1445@rambler.ru>
2023-05-08 15:40:15 +0300

    Magento courses part 1 lessons 11 Created Plugins

--- src/app/code/Panda/Blog/Controller/Post/Detail.php
+++ src/app/code/Panda/Blog/Controller/Post/Detail.php
@@ -3,6 +3,8 @@
 namespace Panda\Blog\Controller\Post;
 
 use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\App\RequestInterface;
+use Magento\Framework\Event\ManagerInterface as EventManager;
 use Magento\Framework\View\Result\Page;
 use Magento\Framework\View\Result\PageFactory;
 
@@ -11,10 +13,16 @@ class Detail implements HttpGetActionInterface
 
     public function __construct(
         private PageFactory $pageFactory,
+        private EventManager $eventManager,
+        private RequestInterface $request,
     ){}
 
     public function execute(): Page
     {
+        $this->eventManager->dispatch('panda_blog_post_detail_view', [
+            'request' => $this->request,
+        ]);
+
         return $this->pageFactory->create();
     }
 }
